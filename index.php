27e1ac40344722e0ce841bf90c6e2e02bfdc5174
ELMATIYOUSSEF <ysf.elmati@gmail.com>
2022-11-14 15:39:44 +0100

    page index && headre

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+   
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="row ">
+
+            <div class="col-3 ">
+            <nav >
+                <ul class=" d-flex flex-column list-inline ">
+                    <li class="text-center py-4  list-group-item">
+                        <a href="#" class="logo  ">
+                            <img class="w-50 " src="./img/Red_Dragon_Logo-removebg-preview.png" alt="">
+                        </a>
+                    </li>
+                    <li  class="text-center text-uppercase fs-3  fw-bold font-monospace  py-4  list-group-item btn btn-secondary"><a href="#" class="text-decoration-none  text-white">
+                            <i class="fas fa-home"></i>
+                            <span class="nav-item">Home </span>
+                        </a>
+                    </li>
+                    <li  class="text-center py-4  text-uppercase fs-3  fw-bold font-monospace   list-group-item btn btn-secondary"><a href="#"  class="text-decoration-none text-white">
+                            <i class="fas fa-user"> </i>
+                            <span class="nav-item">Profile </span>
+                        </a>
+                    </li>
+                    <li  class="text-center py-4 text-uppercase fs-3  fw-bold font-monospace  list-group-item btn btn-secondary"><a href="#" class="text-decoration-none text-white">
+                            <i class="fas fa-home"> </i>
+                            <span class="nav-item">Admin </span>
+                        </a>
+                    </li>
+                    <li class=" text-center  py-4 text-uppercase fs-3  fw-bold font-monospace  list-group-item btn btn-secondary">  <a href="#" class="text-decoration-none text-white">
+                    <i class="fas fa-gamepad"></i>
+                        <span class="nav-item">Games </span>
+                    </a>
+                </li>
+                
+                    <li  class=" text-center py-4 text-uppercase fs-3  fw-bold font-monospace  list-group-item list-group-item-danger btn btn-secondary logout "><a href="#" class="text-decoration-none text-white">
+                            <i class="fas fa-sign-out-alt"></i>
+                            <span class="nav-item"> Log out </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            </div>
+            
+            <div class="col-9">
+                <div class="container overflow-auto">
+                        
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum impedit temporibus voluptatibus corporis dolorem quod quasi debitis! Id veritatis ad, sequi tempore quam sunt quaerat aperiam, deleniti, praesentium similique fugiat.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, eius culpa quos esse veritatis ut repellat deleniti sunt itaque ducimus placeat quae voluptatibus rerum fugit labore saepe mollitia. Debitis, nisi.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolorum, totam, esse a fuga aut nihil quasi, ex iusto delectus ipsam. Modi tempore laudantium architecto eaque est, cupiditate ex doloribus.
+                        </p>
+                    <footer> hi im from footer rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</footer>
+                </div>
+                
+            </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script> 
+
+
+</body>
+
+</html>
