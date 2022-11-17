7bd7841fe281a5f5c116b6af4916cb519fad6cbc
ELMATIYOUSSEF <ysf.elmati@gmail.com>
2022-11-17 09:07:47 +0100

    add page signup

--- pages/gaming.php
+++ pages/gaming.php
@@ -9,9 +9,9 @@ include('../includes/header.php')
             <?php include('../includes/sidebar.php')?>
             </div>
             <div class="col-9 d-flex justify-content-center align-items-center">
-                     <div class="card-group d-flex justify-content-center  ">
+                     <div class="card-group d-flex justify-content-center gap-4 ">
                                         <div class="card">
-                                            <img src="../Assets/img/" class="card-img-top" alt="...">
+                                            <img src="../Assets/img/card1" class="card-img-top" alt="...">
                                             <div class="card-body">
                                             <h5 class="card-title">Card title</h5>
                                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -19,7 +19,7 @@ include('../includes/header.php')
                                             </div>
                                         </div>
                                         <div class="card">
-                                            <img src="../Assets/img/" class="card-img-top" alt="...">
+                                            <img src="../Assets/img/card2" class="card-img-top" alt="...">
                                             <div class="card-body">
                                             <h5 class="card-title">Card title</h5>
                                             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -27,7 +27,7 @@ include('../includes/header.php')
                                             </div>
                                         </div>
                                         <div class="card">
-                                            <img src="../Assets/img/" class="card-img-top" alt="...">
+                                            <img src="../Assets/img/card3" class="card-img-top" alt="...">
                                             <div class="card-body">
                                             <h5 class="card-title">Card title</h5>
                                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
